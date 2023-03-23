07f81bc1c3beec7901cdd2d4ba5052f63d030e5f
AndriiYushkin <yushkinandreyolegovich@gmail.com>
2023-03-23 16:19:12 +0200

    work is don

--- Txt.cpp
+++ Txt.cpp
@@ -1,8 +1,95 @@
-#include <iostream>
+#include "Txt.h"
 
-int main()
+l1::Txt::Txt(const Txt& txt)
 {
-    std::cout << "Hello World!\n";
-}
+	this->Size = txt.size();
 
+	this->ArrFromFile = new std::string[Size];
 
+	for (size_t i = 0; i < Size; i++)
+	{
+		this->ArrFromFile[i] = txt.ArrFromFile[i];
+	}
+};
+
+l1::Txt& l1::Txt::operator=(const Txt& txt)
+{
+	if (this->ArrFromFile)
+	{
+		delete[] this->ArrFromFile;
+	}
+
+	this->Size = txt.size();
+
+	this->ArrFromFile = new std::string[Size];
+
+	for (size_t i = 0; i < Size; i++)
+	{
+		this->ArrFromFile[i] = txt.ArrFromFile[i];
+
+	}
+	return *this;
+};
+
+l1::Txt::Txt(Txt&& txt)
+{
+	this->Size = txt.size();
+	this->ArrFromFile = txt.ArrFromFile;
+
+	txt.ArrFromFile = nullptr;
+	txt.set_size(0);
+};
+
+l1::Txt& l1::Txt::operator=(Txt&& txt)
+{
+	std::string* TempAdress = this->ArrFromFile;
+
+	this->Size = txt.size();
+	this->ArrFromFile = txt.ArrFromFile;
+
+	txt.ArrFromFile = TempAdress;
+	txt.set_size(0);
+
+	return *this;
+};
+
+l1::Txt::Txt(const char* file)
+{
+	std::ifstream F(file, std::ios::in);
+	if (!F)
+	{
+		return;
+	}
+
+	int count = 1;
+	std::string temp;
+	while (!F.eof())
+	{
+		getline(F, temp);	
+		count++;
+	}
+
+	Size = count;
+
+	ArrFromFile = new std::string[Size];
+
+	F.clear();
+	F.seekg(0, F.beg);
+
+	int i = 0;
+	while (!F.eof())
+	{
+		getline(F, ArrFromFile[i]);
+		i++;
+	}
+
+	F.close();
+};
+
+l1::Txt::~Txt()
+{
+	if (this->ArrFromFile)
+	{
+		delete[] this->ArrFromFile;
+	}
+};
--- Txt.h
+++ Txt.h
@@ -1,1 +1,37 @@
 #pragma once
+#include <iostream>
+#include <fstream>
+#include <String>
+namespace l1 {
+
+	class Txt 
+	{
+	public:
+		Txt(){};
+		Txt(const Txt& txt);
+		Txt& operator=(const Txt& txt);
+		Txt(Txt&& txt);
+		Txt& operator=(Txt&& txt);
+		Txt(const char* file);
+
+		size_t size() const { return Size; };
+
+		void set_size(size_t insize) { this->Size = insize; }
+
+		~Txt();
+
+	private:
+		size_t Size = 0;
+		std::string* ArrFromFile = nullptr;
+	};
+
+
+
+
+
+
+
+
+
+
+}
